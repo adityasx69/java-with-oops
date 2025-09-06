fa4daeb658a6ad1f8df138826b59528d6e1586af
Aditya Saraswat <saraswataditya664@gmail.com>
2025-09-06 11:34:04 +0530

    add MathClass.java

---
+++ MathClass.java
@@ -0,0 +1,26 @@
+class MathClass{
+    public static void main(String[] args){
+        
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+        double result1 = Math.pow(2,4);
+        double result2 = Math.abs(-10);
+        double result3 = Math.sqrt(16);
+        double result4 = Math.round(4.32);
+        double result5 = Math.ceil(53.3);
+        double result6 = Math.floor(53.324);
+        double result7 = Math.max(2342,3522);
+        double result8 = Math.min(232,236);
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
+        System.out.println(result6);
+        System.out.println(result7);
+        System.out.println(result8);
+
+    }
+}
