cab0934b034b153161151bf431a48c8b39165068
Aditya Saraswat <saraswataditya664@gmail.com>
2025-10-28 17:13:26 +0530

    add BasicsOfConditionalAndLoops.java

---
+++ kunal-kushwaha/com/first_java_program/BasicsOfConditionalAndLoops.java
@@ -0,0 +1,26 @@
+package com.first_java_program;
+
+public class BasicsOfConditionalAndLoops {
+    static void main() {
+        int a = 10;
+
+        if(a == 10){
+            System.out.println("Hey");
+        }
+
+        System.out.println();
+
+        int i = 1;
+        while(i != 5){
+            System.out.println(i);
+            i++;
+        }
+
+        System.out.println();
+
+        for(int count = 1; count != 5; count++){
+            System.out.println(count);
+        }
+
+    }
+}
