945e67321d2a89cb81210ce180662c3ece6f21bc
Aditya Saraswat <adityasaraswat@Adityas-MacBook-Air.local>
2025-10-28 15:58:40 +0530

    add Sumof2Nums.java

---
+++ kunal-kushwaha/com/aditya/Sumof2Nums.java
@@ -0,0 +1,17 @@
+package com.aditya;
+
+import java.util.Scanner;
+
+public class Sumof2Nums {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first integer: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter the second integer: ");
+        int b = scanner.nextInt();
+
+        int sum = a + b;
+        System.out.println("The sum of the two numbers is " + sum);
+    }
+}
