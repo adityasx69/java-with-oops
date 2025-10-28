23506bc4b6291004699f1340554a38cd7dcebc02
Aditya Saraswat <adityasaraswat@Adityas-MacBook-Air.local>
2025-10-28 15:56:16 +0530

    add Main.java

---
+++ kunal-kushwaha/com/aditya/Main.java
@@ -0,0 +1,11 @@
+package com.aditya;
+
+import java.util.Scanner;
+
+public class Main {
+    static void main() {
+        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLine());
+    }
+}
