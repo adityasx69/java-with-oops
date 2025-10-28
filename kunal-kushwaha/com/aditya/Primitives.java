458aa9e8bcb2cddcfba62fdf268c9e8f8fd83586
Aditya Saraswat <adityasaraswat@Adityas-MacBook-Air.local>
2025-10-28 15:57:54 +0530

    add Primitives.java

---
+++ kunal-kushwaha/com/aditya/Primitives.java
@@ -0,0 +1,12 @@
+package com.aditya;
+
+public class Primitives {
+    static void main() {
+        int a = 10;
+        float b = 121.32f;
+        char c = 'a';
+        double d = 3423.23423424;
+        long l = 341412214L;
+        boolean bool = true;
+    }
+}
